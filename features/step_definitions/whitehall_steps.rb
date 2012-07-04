4b5547bf3d94d2306db9c13cb0ba58aa82196101
Chris Roos <chris@seagul.co.uk>
2012-07-04 12:23:27 +0100

    Consistently bust the cache in the Whitehall feature.
    In trying to track down the request that caused a Smokey Preview build
    failure, I noticed that we weren't busting the cache in our custom
    Whitehall steps.

--- features/step_definitions/whitehall_steps.rb
+++ features/step_definitions/whitehall_steps.rb
@@ -3,9 +3,9 @@ Then /^I should be able to view policies$/ do
 end
 
 def follow_link_to_first_policy_on_policies_page
-  html = get_request("#{@host}/government/policies")
+  html = get_request("#{@host}/government/policies", cache_bust: @bypass_varnish)
   doc = Nokogiri::HTML(html)
   link_to_policy = doc.at('.policy a')
   href = link_to_policy.attributes['href'].value
-  get_request("#{@host}#{href}")
+  get_request("#{@host}#{href}", cache_bust: @bypass_varnish)
 end
