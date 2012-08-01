2ed8c0d89bcc1aafef09788ce15a0dcf5db9f570
Tom Ward <tom@popdog.net>
2012-08-01 10:59:54 +0100

    Check search results are returned from whitehall searches
    Whitehall returns different results for both specialist guidance
    and government content, hence the two tests.

--- features/step_definitions/whitehall_steps.rb
+++ features/step_definitions/whitehall_steps.rb
@@ -6,6 +6,18 @@ Then /^I should be able to view publications$/ do
   follow_link_to_first_publication_on_publications_page
 end
 
+Then /^I should be able to view government search results for "([^"]*)"$/ do |term|
+  html = get_request "#{@host}/government/search?q=#{term}", cache_bust: @bypass_varnish
+  doc = Nokogiri::HTML(html)
+  assert doc.css('.search_results li').count > 0, "No search results returned from /government/search for '#{term}'"
+end
+
+Then /^I should be able to view specialist search results for "([^"]*)"$/ do |term|
+  html = get_request "#{@host}/specialist/search?q=#{term}", cache_bust: @bypass_varnish
+  doc = Nokogiri::HTML(html)
+  assert doc.css('.search_results li').count > 0, "No search results returned from /specialist/search for '#{term}'"
+end
+
 def follow_link_to_first_policy_on_policies_page
   html = get_request("#{@host}/government/policies", cache_bust: @bypass_varnish)
   doc = Nokogiri::HTML(html)
--- features/whitehall.feature
+++ features/whitehall.feature
@@ -17,3 +17,10 @@ Feature: Whitehall
       | /government/world             |
       | /government/search?q=foo      |
       | /BIS                          |
+
+  Scenario: Searching government and specialist guides
+    Given the "whitehall" application has booted
+    And I am testing through the full stack
+    And I force a varnish cache miss
+    Then I should be able to view government search results for "revenue"
+    And I should be able to view specialist search results for "revenue"
