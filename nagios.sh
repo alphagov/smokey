60534f37a068aa6df2b236edb79c3eb7069151de
Magnus Ernstsson <magnus@ernstsson.net>
2012-09-27 15:44:26 +0100

    Added feature and prio parameters to the nagios script

--- nagios.sh
+++ nagios.sh
@@ -4,4 +4,11 @@ cd /opt/smokey;
 
 source /etc/smokey.sh
 
-bundle exec cucumber features --format Cucumber::Formatter::Nagios -t ~@pending -t ~@notnagios
+if [ x$2 = xunprio ]; then
+  ARG="-t ~@high -t ~@medium -t ~@low"
+else
+  ARG="-t @$2"
+fi
+
+bundle exec cucumber features/$1.feature --format Cucumber::Formatter::Nagios -t ~@pending -t ~@notnagios $ARG
+
