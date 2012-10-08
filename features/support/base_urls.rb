b3440434691e36a4f33ba73da7c316e28dd07a1c
James Stewart <jys@ketlai.co.uk>
2012-10-08 08:54:41 +0100

    Alphabetise base urls

--- features/support/base_urls.rb
+++ features/support/base_urls.rb
@@ -14,13 +14,13 @@ def application_base_url(app_name)
   when 'EFG' then "https://efg.#{target_platform}.alphagov.co.uk"
   when 'frontend' then "https://frontend.#{platform}.alphagov.co.uk/"
   when 'licencefinder' then "https://licencefinder.#{platform}.alphagov.co.uk/licence-finder"
+  when 'panopticon' then "https://panopticon.#{platform}.alphagov.co.uk/"
+  when 'publisher' then "https://publisher.#{platform}.alphagov.co.uk/admin"
+  when 'signon' then "https://signon.#{target_platform}.alphagov.co.uk/"
   when 'smartanswers' then "http://smartanswers.#{platform}.alphagov.co.uk/maternity-benefits"
   when 'tariff-backend' then "https://tariff-api.#{platform}.alphagov.co.uk/"
   when 'tariff-frontend' then "https://tariff.#{platform}.alphagov.co.uk/trade-tariff"
   when 'whitehall' then "http://whitehall-frontend.#{platform}.alphagov.co.uk/government"
-  when 'publisher' then "https://publisher.#{platform}.alphagov.co.uk/admin"
-  when 'panopticon' then "https://panopticon.#{platform}.alphagov.co.uk/"
-  when 'signon' then "https://signon.#{target_platform}.alphagov.co.uk/"
   else
     raise "Application '#{app_name}' not recognised, unable to boot it up"
   end
