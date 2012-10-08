a45e50fe4bcb902e26625ff4c48e2d00df9fce2f
James Stewart <jys@ketlai.co.uk>
2012-10-08 09:25:51 +0100

    target_platform and base_url are needed in env

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,5 +2,17 @@ require 'nokogiri'
 require 'capybara/cucumber'
 require 'capybara/mechanize'
 
+def target_platform
+  ENV["TARGET_PLATFORM"] || "preview"
+end
+
+def base_url
+  if target_platform == "production"
+    "https://www.gov.uk"
+  else
+    "https://www.#{target_platform}.alphagov.co.uk"
+  end
+end
+
 Capybara.default_driver = :mechanize
 Capybara.app_host = base_url
