c8aaf6e883ec6bde63ced989852aaa4010ff7f9c
James Abley <james.abley@gmail.com>
2012-10-15 13:36:21 +0100

    Fix EFG CSRF failure
    Skyscape fails correctly. Not sure why EC2 doesn't?

--- features/step_definitions/efg_steps.rb
+++ features/step_definitions/efg_steps.rb
@@ -4,7 +4,16 @@ end
 
 When /^I try to login as a lender user$/ do
   assert ENV["EFG_USERNAME"] && ENV["EFG_PASSWORD"], "Please ensure that the EFG user credentials are available in the environment"
-  @response = post_request "#{efg_base_url}/users/sign_in", :payload => "user[username]=#{ENV["EFG_USERNAME"]}&user[password]=#{ENV["EFG_PASSWORD"]}"
+
+  # Need to do it this way to comply with CSRF protection
+
+  # Not sure if there is a better way to do this?
+  page.driver.browser.agent.add_auth(efg_base_url, ENV['AUTH_USERNAME'], ENV['AUTH_PASSWORD'])
+
+  visit "#{efg_base_url}/users/sign_in"
+  fill_in "Username", :with => ENV["EFG_USERNAME"]
+  fill_in "Password", :with =>ENV["EFG_PASSWORD"]
+  click_button "Sign In"
 end
 
 When /^I visit the EFG home page$/ do
