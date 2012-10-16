0dce56954b27c111a378b1df0e75d9f5fbdd4954
Philip Potter <philipgpotter@gmail.com>
2012-10-16 15:00:52 +0100

    dodge rate limiting for smokey
    Our smokey tests are getting rate limited. Ideally what we should do is get
    nginx to whitelist the local network, but this doesn't seem to be easy without
    using an evil if construct.
    
    The Tengine clone of nginx offers a limit_req_whitelist directive for this purpose:
    http://tengine.taobao.org/document/http_limit_req.html
    but nginx itself doesn't seem to support it.
    
    Therefore, we abuse the X-Forwarded-For header by setting it to a random value, making
    nginx think that the smokey tests are coming from a much larger number of IPs.

--- features/support/curl_helper.rb
+++ features/support/curl_helper.rb
@@ -21,12 +21,14 @@ end
 def fetch_url_with_dns_override(url_string, ip)
   url = URI(url_string)
 
+  rate_limiting_busting_headers = %Q('X-Forwarded-For: 10.0.0.#{rand(256)}')
+
   output_only_headers = "-I"
   authentication = if ENV['AUTH_USERNAME'] && ENV['AUTH_PASSWORD']
     "-u '#{ENV['AUTH_USERNAME']}:#{ENV['AUTH_PASSWORD']}'"
   end
   dns_resolution_override = "--resolve #{url.host}:#{url.port}:#{ip}"
-  cmd = "curl --silent --connect-timeout 5 --max-time 5 #{output_only_headers} #{authentication} #{dns_resolution_override} #{url.to_s}"
+  cmd = "curl --silent --connect-timeout 5 --max-time 5 #{rate_limiting_busting_headers} #{output_only_headers} #{authentication} #{dns_resolution_override} #{url.to_s}"
   puts cmd
   Response.new(`#{cmd}`)
 end
