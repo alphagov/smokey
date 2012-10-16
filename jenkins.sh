641905038ce05aec418f3cf4bafa28d5721271e2
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-10-16 12:25:10 +0100

    Shut up bundler

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,12 @@
-#!/bin/bash -x
+#!/bin/sh
+
+set -x
 
 if [ "$FACTER_govuk_provider" = "skyscape" ]; then
   MYTASK="test:skyscapenetwork"
 else
   MYTASK="test:localnetwork"
 fi
-bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+
+bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment --quiet
 RESTCLIENT_LOG="log/smokey-rest-client.log" bundle exec rake $MYTASK
