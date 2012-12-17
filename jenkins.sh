66cd0176469c92e08af7f21f949cfd4ce8af9d07
Max Griffiths <maxgriff@thoughtworks.com>
2012-12-17 16:43:33 +0000

    setting env var dependencies
    Rather than defaulting to `govuk_setenv`, the standard so far is to
    explicitly export the required variables. This also avoids the need to
    roll out scripts on jenkins boxes

--- jenkins.sh
+++ jenkins.sh
@@ -8,5 +8,7 @@ else
   MYTASK="test:localnetwork"
 fi
 
+export GOVUK_APP_DOMAIN=test.gov.uk
+export GOVUK_ASSET_ROOT=http://static.test.gov.uk
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment --quiet
 RESTCLIENT_LOG="log/smokey-rest-client.log" bundle exec rake $MYTASK
