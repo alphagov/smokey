8c8dd39ffc5843bcc9055d6339226022a46975b7
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-12-29 20:34:33 +0000

    Make smokey nagios check rerun on fail There are two cases when Nagios will generate false positive alerts for Smokey: 1. When the smokey run for that feature is in progress 2. When the smokey cache says the feature is down, but it is already backup. This commit solves both of those problems by checking for the existance of the feature lock file (after the checks for smokey cache age) and re-running cucumber for that feature and priority if it is down - so down state will not be cached for 5 minutes but up state will.

--- nagios_check_cache.sh
+++ nagios_check_cache.sh
@@ -18,6 +18,22 @@ if [ $AGE -gt 1800 ]; then
   exit 3
 fi
 
+if [ -f /tmp/smokey_running_${1} ]; then
+  echo "OK: Smokey run in progress"
+  exit 0
+fi
+
+CHECKOUTPUT=`cat $CHECKFILE`
+CRITICAL=`echo $CHECKOUTPUT | cut -d, -f1 | cut -d\  -f2 | sed 's/ //g' `
+WARNING=`echo $CHECKOUTPUT | cut -d, -f2 | cut -d\  -f3 | sed 's/ //g'`
+if [ "${CRITICAL}${WARNING}" -ne "00" ]; then
+  bundle exec cucumber features/$1.feature --format Cucumber::Formatter::Nagios -t ~@pending -t ~@notskyscape -t ~@notnagios -t @${2} > /tmp/smokey_${1}_${2}
+else
+  echo "OK: $CHECKOUTPUT"
+  exit 0
+fi
+
+
 CHECKOUTPUT=`cat $CHECKFILE`
 CRITICAL=`echo $CHECKOUTPUT | cut -d, -f1 | cut -d\  -f2 | sed 's/ //g' `
 WARNING=`echo $CHECKOUTPUT | cut -d, -f2 | cut -d\  -f3 | sed 's/ //g'`
@@ -31,4 +47,3 @@ else
   echo "OK: $CHECKOUTPUT"
   exit 0
 fi
-
