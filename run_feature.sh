539157b54dd48fdd13136b5cf04f6bb151869ee3
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-01-21 13:33:07 +0000

    Simplify and clean up run_feature.sh
    Again, remove half-baked locking logic, on the basis that this can be handled
    by a custom tool higher up. (See comment about lockrun in cron.sh)

--- run_feature.sh
+++ run_feature.sh
@@ -1,26 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-tmpfeature=${1##*/}
-feature=${tmpfeature%%.*}
-
-if [ -f /tmp/smokey_running_${feature} ]; then
-  echo "Smokey is already running"
+if [ $# -ne 1 ]; then
+  echo "Usage: run_feature.sh <path/to/foo.feature>" >&2
   exit 1
 fi
 
-touch /tmp/smokey_running_${feature}
-
-sleep $(($RANDOM/1000))
+feature=$(basename "$1" .feature)
 
 for priority in urgent high normal low; do
-    tests=`grep -c "@${priority}" $1`
-    if [ $tests -gt 0 ]; then
-      cucumber $1 --format Cucumber::Formatter::Nagios -t ~@pending -t ~@notskyscape -t ~@notnagios -t @${priority} > /tmp/smokey_${feature}_${priority}
-    else
-      echo "Critical: 0, Warning: 0, 0 okay | No tests at this priority" >/tmp/smokey_${feature}_${priority}
-    fi
-done;
-
-rm /tmp/smokey_running_${feature}
+  echo "Running feature '${feature}', ${priority} priority" >&2
+  cucumber "$1" \
+    --format Cucumber::Formatter::Nagios \
+    -t ~@pending \
+    -t ~@notnagios \
+    -t ~@notskyscape \
+    -t "@${priority}" |
+  tee "/tmp/smokey_${feature}_${priority}"
+done
