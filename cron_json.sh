96a33376ca0786743492e811d670068a93b96606
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-01-27 00:03:29 +0000

    cron_json.sh - add error handling and source env

--- cron_json.sh
+++ cron_json.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-CACHE_FILE="$1"
+
+set -e
+
+cd $(dirname "$0")
+
+[ -e /etc/smokey.sh ] && . /etc/smokey.sh
+
+if [ "$1" == "" ]; then
+  echo "Usage: ./cron_json.sh /tmp/smokey.json"
+  exit 1
+else
+  CACHE_FILE="$1"
+fi
+
 TMP_FILE="${CACHE_FILE}.tmp"
 
 rm -f ${TMP_FILE}
