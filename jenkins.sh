848702f984e3111e09e6fbff3730c7ee9d932fed
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-01-29 12:25:42 +0000

    Use govuk_setenv in jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -10,7 +10,5 @@ if [ -z $MYTASK ]; then
   fi
 fi
 
-export GOVUK_APP_DOMAIN=test.gov.uk
-export GOVUK_ASSET_ROOT=http://static.test.gov.uk
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment --quiet
-RESTCLIENT_LOG="log/smokey-rest-client.log" bundle exec rake $MYTASK
+RESTCLIENT_LOG="log/smokey-rest-client.log" govuk_setenv default bundle exec rake $MYTASK
