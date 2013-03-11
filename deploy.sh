afd975ea70eba8ab9978a8282c8860bef3a43254
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-03-11 14:30:04 +0000

    Log to syslog when Smokey is deployed

--- deploy.sh
+++ deploy.sh
@@ -21,4 +21,4 @@ for server in $DEPLOY_TO; do
     chmod 777 log
     rsync -av --delete --exclude='.git' --exclude 'log/*' "$(pwd)/" "deploy@${server}":/opt/smokey
     ssh deploy@${server} 'cd /opt/smokey && bundle install --path $HOME/.bundle/gems --deployment'
-done 
+done logger -p INFO -t jenkins "DEPLOYMENT: ${JOB_NAME} ${BUILD_NUMBER} (${BUILD_URL})"
