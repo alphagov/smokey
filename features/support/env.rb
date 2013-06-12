6a7fe5fa8a804268ca81c43b14320ce7191b47cc
Matthew Ford <matt@bitzesty.com>
2013-06-12 17:54:50 +0100

    Save screenshots
    Added campfire room posting, need to set CAMFIRE env variable with your
    token

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,15 @@
 require 'nokogiri'
 require 'capybara/cucumber'
+require 'capybara/poltergeist'
 require 'capybara/mechanize'
 
 def base_url
   ENV["GOVUK_WEBSITE_ROOT"] || "https://www.preview.alphagov.co.uk"
 end
 
+# Capybara.register_driver :poltergeist do |app|
+  # Capybara::Poltergeist::Driver.new(app, debug: true)
+# end
+Capybara.register_driver :poltergeist
 Capybara.default_driver = :mechanize
 Capybara.app_host = base_url
