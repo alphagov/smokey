a665b783634bce2b378671aad4a89374949a2261
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2014-01-22 17:02:29 +0000

    Remove loop as we only ever deploy to a single machine

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,8 @@ set -eu
 
 DEPLOY_TO="monitoring.management.production"
 
-for server in $DEPLOY_TO; do
-    chmod 777 log
-    rsync -av --delete --exclude='.git' --exclude 'log/*' "$(pwd)/" "deploy@${server}":/opt/smokey
-    ssh deploy@${server} 'cd /opt/smokey && bundle install --path $HOME/.bundle/gems --deployment'
-done
+chmod 777 log
+rsync -av --delete --exclude='.git' --exclude 'log/*' "$(pwd)/" "deploy@${DEPLOY_TO}":/opt/smokey
+ssh deploy@${DEPLOY_TO} 'cd /opt/smokey && bundle install --path $HOME/.bundle/gems --deployment'
+
 logger -p INFO -t jenkins "DEPLOYMENT: ${JOB_NAME} ${BUILD_NUMBER} (${BUILD_URL})"
