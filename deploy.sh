8ae86e3c7ff29d70960aebf47595e062efae4a95
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2014-01-22 17:01:32 +0000

    these machines are dead now

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,7 @@
 #!/bin/sh
 set -eu
 
-case $GOVUK_PROVIDER in
-  sky)
-    DEPLOY_TO="monitoring.management.production"
-  ;;
-  *)
-    case $DEPLOY_ENV in
-      production)
-        DEPLOY_TO="ec2-46-51-143-167.eu-west-1.compute.amazonaws.com"
-      ;;
-      preview)
-        DEPLOY_TO="ec2-176-34-194-237.eu-west-1.compute.amazonaws.com"
-      ;;
-    esac
-  ;;
-esac
+DEPLOY_TO="monitoring.management.production"
 
 for server in $DEPLOY_TO; do
     chmod 777 log
