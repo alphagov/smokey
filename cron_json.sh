1a3f0847ff7416e676b685af959e63180fb21f61
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-03-19 13:08:29 +0000

    Indent properly when using govuk_setenv
    The lack of indentation for the call to bundler did not make it clear
    that this was run within govuk_setenv. That's caused some tail chasing
    when trying to debug whether a failure of Smokey in a new environment is
    related to a network connection issue or a misconfiguration.
    
    This indents the bundle command by 4 spaces to make it clear that it is
    linked to the setenv command and indents the arguments to cucumber by a
    further 4 spaces to make it clear they are related to the bundle exec
    command.

--- cron_json.sh
+++ cron_json.sh
@@ -22,7 +22,7 @@ TMP_FILE="${CACHE_FILE}.tmp"
 
 rm -f ${TMP_FILE}
 /usr/local/bin/govuk_setenv default \
-bundle exec cucumber --format json \
-                     -t ~@pending -t ~@notnagios \
-                     -t ~@notskyscape -t ~@notproduction > ${TMP_FILE} || true
+    bundle exec cucumber --format json \
+        -t ~@pending -t ~@notnagios \
+        -t ~@notskyscape -t ~@notproduction > ${TMP_FILE} || true
 mv ${TMP_FILE} ${CACHE_FILE}
