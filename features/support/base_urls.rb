ee32c5f407d80c4c5712b2bd073e363144bd869c
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-04-02 12:55:53 +0100

    Boot the right application URL for EFG
    When we try to boot EFG, we were asking for efg.app_domain. When you try
    that in staging you are asking for efg.production.alphagov.co.uk which
    points to an external address for production and fails.
    
    By booting efg_base_url we are actually booting the right thing
    (https://www.sflg.gov.uk) which has an internal alias and will work in
    Staging.
    
    /cc @jabley

--- features/support/base_urls.rb
+++ features/support/base_urls.rb
@@ -17,7 +17,7 @@ def application_base_url(app_name)
   when 'backdrop' then "https://www.#{app_domain}"
   when 'calendars' then "https://calendars.#{app_domain}/bank-holidays"
   when 'businesssupportfinder' then "https://businesssupportfinder.#{app_domain}/business-finance-support-finder"
-  when 'EFG' then "https://efg.#{app_domain}"
+  when 'EFG' then efg_base_url
   when 'frontend' then "https://frontend.#{app_domain}/"
   when 'private-frontend' then "https://private-frontend.#{app_domain}/"
   when 'imminence' then "https://imminence.#{app_domain}/"
