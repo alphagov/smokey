c79f463cb6e6722490b0f1ef74cc2d35495bbbc7
Jordan Hatch <jordan@jordanh.net>
2014-08-06 10:43:10 +0100

    Fix the smoke test for the new browse design
    The new browse design includes the heading and description text inside
    the anchor tag, so the existing method doesn't find the section link.
    
    I've fixed this by defining a custom method to click on a section, 
    which looks first for the <h3>, and then fetches the href from its 
    parent.

--- features/step_definitions/frontend_steps.rb
+++ features/step_definitions/frontend_steps.rb
@@ -1,3 +1,9 @@
+When /^I click on the section "(.*?)"$/ do |section_name|
+  link_href = Nokogiri::HTML.parse(@response.body).at_xpath("//h3[text()='#{section_name}']/../@href")
+  link_href.should_not == nil
+  step "I visit \"#{link_href.value}\""
+end
+
 Then /^I should see the services and information section on the homepage$/ do
   html = get_request "#{@host}/", cache_bust: @bypass_varnish
   doc = Nokogiri::HTML(html)
