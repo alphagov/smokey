b3ffe93386cf033c964a4ff1a0a3b596ad569b89
Alex Tomlins <alex@tomlins.org.uk>
2015-10-03 19:35:57 +0100

    Update to work with live content/URLs

--- features/step_definitions/email_campaign_steps.rb
+++ features/step_definitions/email_campaign_steps.rb
@@ -2,14 +2,14 @@ When /^I visit the email campaign press release page$/ do
   if ENV['AUTH_USERNAME']
     page.driver.browser.agent.add_auth(ENV['GOVUK_WEBSITE_ROOT'], ENV['AUTH_USERNAME'], ENV['AUTH_PASSWORD'])
   end
-  visit "#{ENV["GOVUK_WEBSITE_ROOT"]}/test-press-release"
+  visit "#{ENV["GOVUK_WEBSITE_ROOT"]}/lloyds-share-offer"
 end
 
 When /^I visit the email campaign page$/ do
   if ENV['AUTH_USERNAME']
     page.driver.browser.agent.add_auth(ENV['GOVUK_WEBSITE_ROOT'], ENV['AUTH_USERNAME'], ENV['AUTH_PASSWORD'])
   end
-  visit "#{ENV["GOVUK_WEBSITE_ROOT"]}/test-email-campaign"
+  visit "#{ENV["GOVUK_WEBSITE_ROOT"]}/lloydsshares"
 end
 
 Then /^I should be asked to confirm my location$/ do
@@ -24,18 +24,16 @@ When /^I give my location as "(.*?)"$/ do |location|
 end
 
 Then /^I should see the press release$/ do
-  page.should have_content("Rugby World Cup")
+  page.should have_content("Government announces next steps on Lloyds retail sale")
 end
 
 When /^I follow the link to the campaign$/ do
   mechanize_with_referer("#{ENV['GOVUK_WEBSITE_ROOT']}/test-press-release") do
-    #click_link 'www.gov.uk/lloydsshares'
-    click_link 'Go to campaign'
+    click_link 'www.gov.uk/lloydsshares'
   end
 end
 
 Then /^I should see the campaign page$/ do
-  #page.should have_content("Lloyds Banking Group share offer")
-  #page.should have_content("Register your interest in this share offer")
-  page.should have_content("Register your interest")
+  page.should have_content("Lloyds Banking Group share offer")
+  page.should have_content("Register your interest in this share offer")
 end
