05bc60984b9854de3970265ff5c1a7834bec97d1
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-01-05 16:50:06 +0000

    Remove old files
    I don't think the `manual_test` directory has been used in years.
    The JSON output in there definitely isn't needed.
    
    I've also removed 2 scripts in the root directory of the repo which
    aren't used anywhere in our Puppet code or anywhere else that I can
    find, and I don't think anyone runs them manually.

--- manual_test/manual_test_nagios_check_cache.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/python2.6 ../nagios_check_cache.py whitehall high ./smokey.json
-/usr/bin/python2.6 ../nagios_check_cache.py whitehall normal ./smokey.json
-/usr/bin/python2.6 ../nagios_check_cache.py licensing normal ./smokey.json
