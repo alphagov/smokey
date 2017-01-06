d217b72ae3cd176cc84874dd58bfd6e4bad3a94c
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2017-01-06 15:13:18 +0000

    Remove unused frontend step

--- features/step_definitions/frontend_steps.rb
+++ features/step_definitions/frontend_steps.rb
@@ -4,12 +4,6 @@ When /^I click on the section "(.*?)"$/ do |section_name|
   step "I visit \"#{link_href.value}\""
 end
 
-Then /^I should see the services and information section on the homepage$/ do
-  html = get_request "#{@host}/", cache_bust: @bypass_varnish
-  doc = Nokogiri::HTML(html)
-  assert doc.css('#services-and-information')
-end
-
 Then /^I should see an input field for postcode$/ do
   @response.body.should have_field('postcode')
 end
