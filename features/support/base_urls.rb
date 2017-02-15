dcdb20630accdca0aa2216b38cfff83035b6b343
Isabell Long <isabell@issyl0.co.uk>
2017-02-15 13:34:56 +0000

    Remove Panopticon-related steps

--- features/support/base_urls.rb
+++ features/support/base_urls.rb
@@ -20,7 +20,6 @@ def application_base_url(app_name)
   when 'licencefinder' then "https://licencefinder.#{app_domain}/licence-finder"
   when 'licensing' then "https://licensify.#{app_domain}/apply-for-a-licence"
   when 'local-links-manager' then "https://local-links-manager.#{app_domain}"
-  when 'panopticon' then "https://panopticon.#{app_domain}/"
   when 'public-contentapi' then "https://www.#{app_domain}/api/tags.json" # this should be changed to a Content API 'homepage' when we have one
   when 'publisher' then "https://publisher.#{app_domain}/admin"
   when 'signon' then "https://signon.#{app_domain}/"
