b40f5d01b9ea900e255a2a56aa7cde3638c930f9
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2017-09-05 16:32:26 +0000

    Check what facets look like in finder pages
    This tests for issues like this:
    https://docs.publishing.service.gov.uk/manual/incorrect-content-in-search-or-navigation.html#there-are-blank-options-on-finder-pages
    
    The root cause in this case is rummager holding inconsistent data.  It knows
    pages are tagged to people/organisations/topics but sometimes it's missing the
    thing being tagged to.
    
    In this case finder frontend just renders the thing with a blank title.
    
    We could handle this better in finder frontend, but I think if the labels are ever
    blank it should fail smokey, as this means the finder is not working properly.

--- features/step_definitions/finder_frontend_steps.rb
+++ features/step_definitions/finder_frontend_steps.rb
@@ -1,3 +1,22 @@
 Then /^I should see an input field to search$/ do
   page.body.should have_field('keywords')
 end
+
+And /^I should see an? (open|closed) facet titled "(.*?)" with non-blank values$/ do |open_closed, title|
+  facet = page.find('.govuk-option-select') { |elem| elem.find('.js-container-head').has_text?(title)}
+
+  if open_closed == "open"
+    expect(facet[:class].split(" ")).not_to include("js-closed")
+  else
+    expect(facet[:class].split(" ")).to include("js-closed")
+
+    # Facet options don't exist when you're not looking at them :(
+    facet.find('button').click
+  end
+
+  labels = facet.all('.options-container label').map(&:text)
+  blank_labels = labels.select { |label| label.strip.empty? }
+
+  expect(labels).not_to be_empty
+  expect(blank_labels).to be_empty
+end
