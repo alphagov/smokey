1350735fa6778a30f3dad1d70065b2fc9e1cc0d1
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2018-02-27 12:08:44 +0000

    Fix failing feedback selector

--- features/step_definitions/csv_steps.rb
+++ features/step_definitions/csv_steps.rb
@@ -1,5 +1,7 @@
 Then /^JavaScript should run without any errors$/ do
-  # If the `gem-c-feedback__prompt-link` element exists then JavaScript
-  # is running on the page and there are no SRI or similar errors
+  # If the `gem-c-feedback__prompt-link` element exists and the
+  # page displays the text 'Is there anything wrong with this page?'
+  # then JavaScript is running on the page and there are no SRI or similar errors
+  expect(page).to have_content("Is there anything wrong with this page?")
   expect(page).to have_selector(".gem-c-feedback__prompt-link[aria-expanded='false']")
 end
