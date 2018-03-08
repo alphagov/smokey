fcfa9acc1850732880e2508f581c22ad7b844994
Chris Roos <chris@seagul.co.uk>
2018-03-08 11:18:40 +0000

    Add scenario to ensure draft-assets is protected by signon
    I noticed that visiting draft-assets is currently redirecting us to the
    internal signon URL instead of the external URL (this has been fixed in
    asset-manager PR 512[1]).
    
    Adding this scenario should help us avoid similar problems in future.
    
    I've had to add the `@draft-assets` tag and use an `Around` filter to
    set the Capybara host to use draft-assets. This is very similar to how
    we're using the `@draft` tag.
    
    I initially attempted to use a combination of `Given I am testing
    "draft-assets"` and `When I request "/path"` but that uses RestClient to
    make the request and I don't seem to be able to get hold of the URL that
    we end up on.
    
    [1]: https://github.com/alphagov/asset-manager/pull/512

--- features/step_definitions/signon_steps.rb
+++ features/step_definitions/signon_steps.rb
@@ -7,3 +7,9 @@ When /^I try to login as a user$/ do
   fill_in "Passphrase", :with => ENV["SIGNON_PASSWORD"]
   click_button "Sign in"
 end
+
+Then(/^I should be redirected to signon$/) do
+  signon_url = application_external_url("signon")
+
+  expect(page.current_url).to match(signon_url)
+end
