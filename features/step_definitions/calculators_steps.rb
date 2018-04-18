205be028a0fa7b09f168690e016efc18e8c976c6
Steve Laing <steve.laing@gmail.com>
2018-04-18 14:37:25 +0100

    Check for previous tax year option
    Adds a check for the previous tax year showing in the child
    benefit tax calculator. This has been missed when applying
    April uprating changes.

---
+++ features/step_definitions/calculators_steps.rb
@@ -0,0 +1,13 @@
+def previous_tax_years
+  today = Date.today
+  threshold = Date.new(today.year, 4, 5)
+  years = [today.year - 1, today.year]
+  years = years.map { |y| y - 1 } if today < threshold
+  years
+end
+
+Then(/^I should be able to see the previous tax year$/) do
+  within(".tax-year") do
+    page.should have_content(previous_tax_years.join(" to "))
+  end
+end
