79e8bf90edd199b415a27db79d769ee8b172258b
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2018-07-16 13:43:46 +0100

    Make licensify-admin consistent with other paths
    This commit makes tests on `licensify-admin` with others, where the default path is defined centrally.

--- features/step_definitions/licensing.rb
+++ features/step_definitions/licensing.rb
@@ -1,4 +1,4 @@
 When /^I login to Licensify$/ do
-  visit_path "#{Plek.new.external_url_for('licensify-admin')}/login"
-  click_button 'Login'
+  visit_path application_external_url("licensify-admin")
+  click_button "Login"
 end
