6aac21ca0ef065d30d0037f9b5f4d1abfb292303
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2018-07-20 15:28:55 +0100

    Swap from PhantomJS to Selenium/Chrome
    This commit switches the browser used to run the Smokey tests from PhantomJS to Selenium/Chrome.
    
    As a side-effect, it also sets up the Selenium configuration and includes BrowserMob Proxy, used to add HTTP request headers and log responses.

--- features/support/visiting_pages.rb
+++ features/support/visiting_pages.rb
@@ -1,10 +1,6 @@
 require 'base64'
 
 def visit_path(path)
-  if ENV['AUTH_USERNAME'] && ENV['AUTH_PASSWORD']
-    page.driver.basic_authorize(ENV['AUTH_USERNAME'], ENV['AUTH_PASSWORD'])
-  end
-
   if path.match(%r[\?])
     visit "#{path}&cachebust=#{rand.to_s}"
   else
