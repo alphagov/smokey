274f17d60d8b39523803c562044d5234a695fc61
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2018-09-10 11:08:03 +0100

    Fix proxy server config for chrome driver

--- features/support/env.rb
+++ features/support/env.rb
@@ -58,12 +58,12 @@ Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     acceptInsecureCerts: true,
     loggingPrefs: { browser: "ALL" },
-    proxy: { type: :manual, ssl: "#{proxy.host}:#{proxy.port}" }
   )
 
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument("--headless")
   options.add_argument("--disable-gpu")
+  options.add_argument(%(--proxy-server="http=#{proxy.host}:#{proxy.port};https=#{proxy.host}:#{proxy.port}"))
 
   Capybara::Selenium::Driver.new(
     app,
