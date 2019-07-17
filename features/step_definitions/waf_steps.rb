c141cfabbcca01c86a818bfd2de3cfc8432afe71
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-07-17 14:19:07 +0100

    Fix whitespace formatting

--- features/step_definitions/waf_steps.rb
+++ features/step_definitions/waf_steps.rb
@@ -1,11 +1,10 @@
-
 Given /^I set header ([^\s]+) to ([^\s]+)/ do |header_name, header_value|
-	@headers ||= {}
-	@headers[header_name.to_sym] = header_value
+  @headers ||= {}
+  @headers[header_name.to_sym] = header_value
 end
 
 When /^I send a (\w+) request to "(.*?)"$/ do |method, path|
-	@response = do_http_request("#{@host}#{path}", method.downcase.to_sym, default_request_options.merge({:headers => @headers}))
+  @response = do_http_request("#{@host}#{path}", method.downcase.to_sym, default_request_options.merge({:headers => @headers}))
 end
 
 Then /^the response status should be "([^"]*)"$/ do |status|
--- features/waf.feature
+++ features/waf.feature
@@ -1,9 +1,9 @@
 @aws @local-network
 Feature: WAF
-    Tests to ensure expected WAF rules are in place
+  Tests to ensure expected WAF rules are in place
 
-    @high
-    Scenario: Check that the X-Always-Block rule is in place
-      Given I set header X-Always-Block to true
-      When I send a GET request to /
-      Then the response status should be 403
+  @high
+  Scenario: Check that the X-Always-Block rule is in place
+    Given I set header X-Always-Block to true
+    When I send a GET request to /
+    Then the response status should be 403
