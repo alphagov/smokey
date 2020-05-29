2e18a2db4372c9ddbe978a6e569b003ab14fd364
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-05-29 15:50:19 +0100

    Remove redundant Rbenv workaround
    This doesn't seem to be a problem anymore.
    
    Original commit: e8941194156df76d801f7c67e1dc943e6fd2fee0

--- jenkins.sh
+++ jenkins.sh
@@ -6,13 +6,5 @@ if [ -z $MYTASK ]; then
   MYTASK="test:production"
 fi
 
-# This removes rbenv shims from the PATH where there is no
-# .ruby-version file. This is because certain gems call their
-# respective tasks with ruby -S which causes the following error to
-# appear: ruby: no Ruby script found in input (LoadError).
-if [ ! -f .ruby-version ]; then
-  export PATH=$(printf $PATH | awk 'BEGIN { RS=":"; ORS=":" } !/rbenv/' | sed 's/:$//')
-fi
-
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 RESTCLIENT_LOG="log/smokey-rest-client.log" govuk_setenv smokey bundle exec rake $MYTASK
