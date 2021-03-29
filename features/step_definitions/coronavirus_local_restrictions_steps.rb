5a6696780d83603dac883e2eec101372b035926a
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2021-03-29 07:33:08 +0100

    Update redirect link for postcode checker
    - In line with changes to national coronavirus restrictions on 29/03/2021

--- features/step_definitions/coronavirus_local_restrictions_steps.rb
+++ features/step_definitions/coronavirus_local_restrictions_steps.rb
@@ -3,5 +3,5 @@ When "I visit the postcode checker" do
 end
 
 Then "I should be redirected to the national restrictions page" do
-  expect(current_path).to eql("/guidance/national-lockdown-stay-at-home")
+  expect(current_path).to eql("/guidance/covid-19-coronavirus-restrictions-what-you-can-and-cannot-do")
 end
