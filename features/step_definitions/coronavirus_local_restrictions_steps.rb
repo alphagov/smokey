851152b812fdd3282d1074ede57077ca9241389c
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-05-06 17:56:16 +0100

    Remove coronavirus local restrictions test
    The code for the postcode checker is being removed and the content item will
    be unpublished in the normal way, so we no longer need this test.

--- features/step_definitions/coronavirus_local_restrictions_steps.rb
+++
@@ -1,7 +0,0 @@
-When "I visit the postcode checker" do
-  visit_path "/find-coronavirus-local-restrictions"
-end
-
-Then "I should be redirected to the national restrictions page" do
-  expect(current_path).to eql("/guidance/covid-19-coronavirus-restrictions-what-you-can-and-cannot-do")
-end
