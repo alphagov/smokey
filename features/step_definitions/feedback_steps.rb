645be0cad63f4aca008753d60b6c074a41c45fbe
Kevin Dew <kevindew@me.com>
2022-03-24 17:11:41 +0000

    Quick fix to remove jQuery expectation
    We're removing jQuery and this code is now causing a test to fail. Removing jQuery will fix it.

--- features/step_definitions/feedback_steps.rb
+++ features/step_definitions/feedback_steps.rb
@@ -1,5 +1,5 @@
 When /^I input malicious code in the (.*) field$/ do |field|
-  page.execute_script('$("form").attr("novalidate", "novalidate")')
+  page.execute_script("document.querySelector('.contact-form').setAttribute('novalidate', 'novalidate')")
   find("#email").set("<script>alert(document.cookie)</script>")
   click_button("Send message")
 end
