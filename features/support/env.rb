5ffd3354803d372896b16eac921961f0ee0cf642
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2022-04-12 09:59:36 +0100

    Try moving blacklist URLs to ignored errors
    The blacklisting meant we didn't actually know if requests to GA
    were successful [^1]. In terms of whether that use case is real:
    bots crawl GOV.UK all the time, so we shouldn't be concerned about
    a small amount of Smokey traffic.
    
    The other use cases are only concerned with ignoring errors, which
    we can equally do with log filters.
    
    [^1]: https://github.com/alphagov/smokey/blob/71f157fa17efe9d561757051975844d794719659/features/step_definitions/analytics.rb#L29

--- features/support/env.rb
+++ features/support/env.rb
@@ -59,16 +59,6 @@ if ENV["AUTH_USERNAME"] && ENV["AUTH_PASSWORD"]
   )
 end
 
-# Blacklist YouTube to prevent cross-site errors
-proxy.blacklist(/^https:\/\/www\.youtube\.com/i, 200)
-proxy.blacklist(/^https:\/\/s\.ytimg\.com/i, 200)
-
-# To avoid sending events to Google Analytics
-proxy.blacklist(/^https:\/\/www\.google\-analytics\.com/i, 200)
-
-# Licensify admin doesn't have favicon.ico so block requests to prevent errors
-proxy.blacklist(/^https:\/\/licensify-admin(.*)\.publishing\.service\.gov\.uk\/favicon\.ico$/i, 200)
-
 # Use Chrome in headless mode
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
@@ -101,4 +91,8 @@ Capybara::Chromedriver::Logger.filter_levels = %i(debug info warning)
 Capybara::Chromedriver::Logger.filters = [
   /Failed to load resource/i,
   /The target origin provided/i,
+  /https:\/\/www\.youtube\.com/i,
+  /https:\/\/s\.ytimg\.com/i
+  /https:\/\/www\.google\-analytics\.com/i
+  /https:\/\/licensify-admin(.*)\.publishing\.service\.gov\.uk\/favicon\.ico/i
 ]
