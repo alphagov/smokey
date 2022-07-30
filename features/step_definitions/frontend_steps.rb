4423f1894c109e7ed9c32361003aaa83cc82f88e
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2022-07-30 19:04:27 +0100

    Fix collections test
    We are removing the Miller Column style template, and the test needs to reflecgt change in markup.

--- features/step_definitions/frontend_steps.rb
+++ features/step_definitions/frontend_steps.rb
@@ -1,5 +1,5 @@
 When /^I click on the section "(.*?)"$/ do |section_name|
-  link_href = Nokogiri::HTML.parse(page.body).at_xpath("//h3[text()='#{section_name}']/../@href")
+  link_href = Nokogiri::HTML.parse(page.body).at_xpath("//a[text()='#{section_name}']/@href")
   expect(link_href).not_to be_nil
   step "I visit \"#{link_href.value}\""
 end
