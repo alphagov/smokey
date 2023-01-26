3e429a829974784f8776aad4642f28b948260b98
AshGDS <8880610+AshGDS@users.noreply.github.com>
2023-01-26 09:57:29 +0000

    Add script to tests to test integration failures

--- features/step_definitions/analytics.rb
+++ features/step_definitions/analytics.rb
@@ -1,19 +1,23 @@
 Then /^search analytics for "(.*)" are reported$/ do |term|
+  page.execute_script("if(window.jasmine){console.error('error: jasmine exists so analytics did not load')} else if (!window.GOVUK.analytics) { console.error('universal analytics has not loaded in yet')}")
   sought = "dp=#{CGI::escape("/search/all?keywords=#{term.sub(' ', '+')}")}"
   expect(browser_has_analytics_request_containing sought).to be(true)
 end
 
 Then /^the "(.*)" event is reported$/ do |event|
+  page.execute_script("if(window.jasmine){console.error('error: jasmine exists so analytics did not load')} else if (!window.GOVUK.analytics) { console.error('universal analytics has not loaded in yet')}")
   sought = "ec=#{event}"
   expect(browser_has_analytics_request_containing sought).to be(true)
 end
 
 Then /^the "(.*)" event for result (.*) is reported$/ do |event, n|
+  page.execute_script("if(window.jasmine){console.error('error: jasmine exists so analytics did not load')} else if (!window.GOVUK.analytics) { console.error('universal analytics has not loaded in yet')}")
   sought = "ec=#{event}&ea=#{n}"
   expect(browser_has_analytics_request_containing sought).to be(true)
 end
 
 Then /^the page view should be tracked$/ do
+  page.execute_script("if(window.jasmine){console.error('error: jasmine exists so analytics did not load')} else if (!window.GOVUK.analytics) { console.error('universal analytics has not loaded in yet')}")
   sought = "t=pageview"
   expect(browser_has_analytics_request_containing sought).to be(true)
 end
