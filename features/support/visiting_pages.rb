b905fdafafd5094fd0688a76d987dd658260d6c2
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-01-03 08:35:58 +0000

    Make the differences clearer in the 'visit_path' method
    At first glance, the 'if' and 'else' expressions looked identical.
    This commit extracts the difference out to a well named variable
    instead.

--- features/support/visiting_pages.rb
+++ features/support/visiting_pages.rb
@@ -1,9 +1,6 @@
 require 'base64'
 
 def visit_path(path)
-  if path.match(%r[\?])
-    visit "#{path}&smokey_cachebust=#{rand.to_s}"
-  else
-    visit "#{path}?smokey_cachebust=#{rand.to_s}"
-  end
+  url_param_joiner = path.match(%r[\?]) ? "&" : "?"
+  visit "#{path}#{url_param_joiner}smokey_cachebust=#{rand.to_s}"
 end
