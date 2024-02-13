d4d34e1fc9aa51d0601b7fed87695fb7f6b5414d
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-02-13 15:36:54 +0000

    Add a Mainstream Publisher non-production test
    The latest version of the mongo driver currently causes the app to fail
     in deployed environments when saving to the database. This issue does
     not occur locally, and so we have no way of testing the driver update
     is safe without manual deployment and testing.
    
    Add a test that will attempt to create an artefact in Mainstream
    Publisher and then delete it. If this test fails it will mean that the
     artefact created will need manual deletion before the Smokey test can
     pass again, but this is deemed acceptable by the team as it should be a
     rare occurrence.
    
    We will run this test in integration and staging, but NOT production, to
     avoid adding junk data into the production database (and the data in
     integration and staging is overwritten overnight).

--- features/apps/publisher.feature
+++ features/apps/publisher.feature
@@ -7,3 +7,12 @@ Feature: Publisher
     And I go to the "publisher" landing page
     Then I should see "Publisher"
     And I should see Publisher's publication index
+
+  @notproduction
+  Scenario: Can add and delete an artefact in publisher
+    When I go to the "publisher" landing page
+    And I try to login as a user
+    And I go to the "publisher" landing page
+    And I add an artefact
+    And I delete the artefact
+    Then I should see that the edition has been deleted
---
+++ features/step_definitions/publisher_steps.rb
@@ -0,0 +1,17 @@
+When /^I add an artefact$/ do
+  click_link "Add artefact"
+  fill_in "Title", with: "Smokey Guide"
+  fill_in "Slug", with: "smokey-guide"
+  select "Guide", from: "Format"
+  click_button "Save and go to item"
+  expect(page).to have_selector("h1", text: "Smokey Guide")
+end
+
+When /^I delete the artefact$/ do
+  click_link "Admin"
+  click_button "Delete this edition – #1"
+end
+
+Then /^I should see that the edition has been deleted$/ do
+  expect(page).to have_content("Edition deleted")
+end
